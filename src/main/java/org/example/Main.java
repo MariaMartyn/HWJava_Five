23b3dd707ce1f081218a8a5b11cf5151e91036b5
MariaMartyniuk <mariyagrabko@gmail.com>
2023-04-13 18:38:27 +1000

    Master

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,8 @@ import static org.example.HeapSort.printArray;
 
 public class Main {
     public static void main(String[] args) {
-    phoneBook();
-    //heapSortTask();
+    //phoneBook();
+    heapSortTask();
     }
 /**
     Задача 1. * Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь
